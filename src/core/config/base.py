21931b746ce4e65182db992a63741919f5d7e070
selezGit <pwselezard@gmail.com>
2021-08-18 11:23:36 +0300

    fix: fix small busg, update styles (#1)

--- src/core/config/base.py
+++ src/core/config/base.py
@@ -1,24 +1,58 @@
 import os
 
-from openpyxl.styles import PatternFill
+from openpyxl.styles import NamedStyle, PatternFill
+from openpyxl.styles.borders import Border, Side
+from openpyxl.styles.fonts import Font
 
 BASE_URL = 'https://www.moex.com/export/derivatives/currency-rate.aspx?language=ru'
 FILE_NAME = 'test.xlsx'
 PARSE_LIST = ['USD/RUB', 'EUR/RUB']
 
-# patterns
+# openpyxl styles
+HEADER = PatternFill(start_color='4169E1',
+                     end_color='4169E1',
+                     fill_type='solid',)
 
-BASE = PatternFill(start_color='00FFCC99',
-                   end_color='00FFCC99',
+BASE = PatternFill(start_color='E0FFFF',
+                   end_color='E0FFFF',
                    fill_type='solid',)
 
-RED = PatternFill(start_color='00FF0000',
-                  end_color='00FF0000',
+RED = PatternFill(start_color='F08080',
+                  end_color='F08080',
                   fill_type='solid',)
 
-GREEN = PatternFill(start_color='0000FF00',
-                    end_color='0000FF00',
+GREEN = PatternFill(start_color='C2D69B',
+                    end_color='C2D69B',
                     fill_type='solid',)
+
+
+THIN_BORDER = Border(left=Side(style='thin'),
+                     right=Side(style='thin'),
+                     top=Side(style='thin'),
+                     bottom=Side(style='thin'))
+
+HEADER_STYLE = NamedStyle(
+    name='header',
+    border=THIN_BORDER,
+    fill=HEADER,
+    font=Font(color='00FFFFFF')
+)
+BASE_STYLE = NamedStyle(
+    name='base',
+    border=THIN_BORDER,
+    fill=BASE
+)
+RED_STYLE = NamedStyle(
+    name='red',
+    border=THIN_BORDER,
+    fill=RED
+)
+GREEN_STYLE = NamedStyle(
+    name='green',
+    border=THIN_BORDER,
+    fill=GREEN,
+)
+
 
 # email
 SMTP_SERVER = os.getenv('SMTP_SERVER')
--- src/save_to_excel.py
+++ src/save_to_excel.py
@@ -1,7 +1,4 @@
 from openpyxl import Workbook, load_workbook
-from openpyxl.styles.numbers import (FORMAT_CURRENCY_EUR_SIMPLE,
-                                     FORMAT_CURRENCY_USD_SIMPLE,
-                                     FORMAT_NUMBER_00)
 from openpyxl.utils import get_column_letter
 
 from src.core import config
@@ -12,35 +9,48 @@ def save_to_excel(data, offset, exchange):
         wb = load_workbook(config.FILE_NAME)
     except FileNotFoundError:
         wb = Workbook()
+
     ws = wb.active
 
     if 'EUR' in exchange:
-        currency = FORMAT_CURRENCY_EUR_SIMPLE
+        currency = '#,#####0.0000€' 
     else:
-        currency = FORMAT_CURRENCY_USD_SIMPLE
+        currency = '#,#####0.0000$'
 
-    ws.cell(column=offset+1, row=1, value='Дата').fill = config.BASE
-    ws.cell(column=offset+2, row=1, value='Курс').fill = config.BASE
-    ws.cell(column=offset+3, row=1, value='Изменение').fill = config.BASE
+    ws.cell(column=offset+1,
+            row=1,
+            value='Дата',
+            ).style = config.HEADER_STYLE
+    ws.cell(column=offset+2,
+            row=1,
+            value='Курс',
+            ).style = config.HEADER_STYLE    
+        
+    ws.cell(column=offset+3,
+            row=1,
+            value='Изменение',
+            ).style = config.HEADER_STYLE
 
     for row, date in enumerate(data.keys()):
         exchange_rate = float(data[date][0]['value'])
         difference = exchange_rate - float(data[date][1]['value'])
+
         color = config.RED if difference <= 0 else config.GREEN
 
-        ws.cell(column=offset+1, row=row+2, value=date).fill = config.BASE
-        cell_rate = ws.cell(column=offset+2, row=row+2, value=exchange_rate)
-        cell_rate.fill = config.BASE
+        cell_date = ws.cell(column=offset+1, row=row+2,
+                value=date).style = config.BASE_STYLE
+        cell_date.number_format = currency
+        
+        cell_rate = ws.cell(column=offset+2,
+                            row=row+2,
+                            value=exchange_rate,
+                            ).style = config.BASE_STYLE
         cell_rate.number_format = currency
 
         cell_diff = ws.cell(column=offset+3, row=row+2, value=difference)
-        cell_diff.fill = color
         cell_diff.number_format = currency
-
-    # авто выравнивание
-    for i in range(1, ws.max_column+1):
-        ws.column_dimensions[get_column_letter(i)].bestFit = True
-        ws.column_dimensions[get_column_letter(i)].auto_size = True
+        cell_diff.border = config.THIN_BORDER
+        cell_diff.fill = color
 
     wb.save(config.FILE_NAME)
 
@@ -48,7 +58,6 @@ def save_to_excel(data, offset, exchange):
 def update_data():
     wb = load_workbook(config.FILE_NAME)
     ws = wb.active
-
     for index, row in enumerate(ws.iter_rows()):
         usd, eur = 0, 0
         for cell in row:
@@ -57,13 +66,23 @@ def update_data():
             elif cell.column == 5:
                 eur = cell.value
         try:
-            result = eur/usd
+            mid_market_rate = eur/usd
         except:
-            result = ''
+            mid_market_rate = ''
 
-        res_col = ws.cell(column=7, row=index+1, value=result)
-        res_col.fill = config.BASE
-        res_col.number_format = FORMAT_NUMBER_00
+        res_col = ws.cell(column=7,
+                          row=index+1,
+                          value=mid_market_rate,
+                          ).style = config.BASE_STYLE
+        res_col.number_format = '#,#####0.00000'
+
+    ws.cell(column=7, row=1, value='Средний курс').style = config.HEADER_STYLE
+
+    # авто выравнивание
+    for i in range(1, ws.max_column+1):
+        ws.column_dimensions[get_column_letter(i)].bestFit = True
+        ws.column_dimensions[get_column_letter(i)].auto_size = True
+
 
     wb.save(config.FILE_NAME)
 
